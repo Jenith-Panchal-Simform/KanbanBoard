331160ceac6e66767fe56a30a2f3f5d6b40ae105
Jenith-Panchal-Simform <jenith.panchal@simformsolutions.com>
2026-04-03 14:23:01 +0530

    feat:add drag if content scrollable

--- index.js
+++ index.js
@@ -1,54 +1,65 @@
 let cards = document.querySelectorAll(".card");
 
 cards.forEach((current) => {
-
   current.onmousedown = function (e) {
-
     let shiftX = e.clientX - current.getBoundingClientRect().left;
     let shiftY = e.clientY - current.getBoundingClientRect().top;
 
-    let droppableBelow = null; 
+    let droppableBelow = null;
     // move to body for free movement
 
     function moveAt(pageX, pageY) {
       let newX = pageX - shiftX;
       let newY = pageY - shiftY;
-
+      //restrict the drag to the window if not scroll .
+      // let maxY = document.documentElement.clientHeight - current.offsetHeight;
+      // newY = Math.max(0, Math.min(newY, maxY));
       let maxX = document.documentElement.clientWidth - current.offsetWidth;
-      let maxY = document.documentElement.clientHeight - current.offsetHeight;
-
       newX = Math.max(0, Math.min(newX, maxX));
-      newY = Math.max(0, Math.min(newY, maxY));
-
+      newY = Math.max(0, newY);
       current.style.left = newX + "px";
       current.style.top = newY + "px";
     }
 
     function onMouseMove(e) {
       current.style.position = "absolute";
-    current.style.zIndex = 1000;
-    document.body.append(current); 
+      current.style.zIndex = 1000;
+      document.body.append(current);
       moveAt(e.pageX, e.pageY);
+
+      let edgeThreshold = 50;
+      let scrollSpeed = 20;
+
+      let container = document.querySelector(".board__wrapper");
+
+      //scroll the page when 
+      // Right edge
+      if (e.clientX > window.innerWidth - edgeThreshold) {
+        container.scrollLeft += scrollSpeed;
+      }
+
+      // Left edge
+      if (e.clientX < edgeThreshold) {
+        container.scrollLeft -= scrollSpeed;
+      }
 
       //  hide element to detect what's below it
       current.hidden = true;
       let elemBelow = document.elementFromPoint(e.clientX, e.clientY);
       current.hidden = false;
-        
+
       if (!elemBelow) return;
       // get nearest droppable container
-      droppableBelow=elemBelow.closest(".droppable")
-      if(!droppableBelow)
-      {
-          droppableBelow = elemBelow.querySelector(".droppable");
+      droppableBelow = elemBelow.closest(".droppable");
+      if (!droppableBelow) {
+        droppableBelow = elemBelow.querySelector(".droppable");
       }
     }
 
     document.addEventListener("mousemove", onMouseMove);
-     function mouseUpHandler() {
-
+    function mouseUpHandler() {
       document.removeEventListener("mousemove", onMouseMove);
-      
+
       // if dropped inside a column
       if (droppableBelow) {
         droppableBelow.append(current);
@@ -60,7 +71,7 @@ cards.forEach((current) => {
         current.style.zIndex = "";
       }
     }
-    document.addEventListener("mouseup",mouseUpHandler, { once: true });
+    document.addEventListener("mouseup", mouseUpHandler, { once: true });
 
     // disable default drag behavior
     current.ondragstart = () => false;
